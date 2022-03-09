6fe5e93103ec72833f4a594acb8cc92f21ac054c
IndreATKO <indre.atkociune@gmail.com>
2022-03-10 00:30:28 +0100

    Objects Utils

--- app/src/main/java/com/example/textcounterindre/MainActivity.java
+++ app/src/main/java/com/example/textcounterindre/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.textcounterindre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.textcounterindre.utils.TextUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    EditText txtMain;
+    Spinner spDropDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvResult = findViewById(R.id.tvResult);
+        this.txtMain = findViewById(R.id.txtMain);
+        this.spDropDown = findViewById(R.id.spDropDown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.counting_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spDropDown.setAdapter(adapter);
+
+    }
+
+    public void onBtnCalculateClick(View view) {
+        if (this.spDropDown.getSelectedItem().toString().equalsIgnoreCase("Words")) {
+//to do
+        } else {
+            int charsCount = TextUtils.getCharsCount(this.txtMain.getText().toString());
+            String charsCountFormatted = String.valueOf(charsCount);
+            this.tvResult.setText(charsCountFormatted);
+
+//            String content = this.txtMain.getText().toString()
+//            int charsCount = content.length();
+//            String charsCountFormatted = String.valueOf(charsCount);
+//            this.tvResult.setText(charsCountFormatted);
+        }
     }
 }
---
+++ app/src/main/java/com/example/textcounterindre/utils/TextUtils.java
@@ -0,0 +1,7 @@
+package com.example.textcounterindre.utils;
+
+public class TextUtils {
+    public static int getCharsCount(String input) {
+        return input.length();
+    }
+}
